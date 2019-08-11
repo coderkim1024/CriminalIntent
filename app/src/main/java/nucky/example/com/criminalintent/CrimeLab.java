b3a47ca7d1fdf15c244c9b53c2042bde0ca58220
coderkim1024 <coderkim1024@gmail.com>
2019-08-11 22:37:29 +0800

    Create CrimeListFragment and CrimeListActivity

---
+++ app/src/main/java/nucky/example/com/criminalintent/CrimeLab.java
@@ -0,0 +1,45 @@
+package nucky.example.com.criminalintent;
+
+import android.content.Context;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.UUID;
+
+/**
+ * 单例设计模式
+ */
+public class CrimeLab {
+    private static CrimeLab sCrimeLab;
+    private List<Crime> mCrimes;
+    //唯一的获取方法
+    public static CrimeLab get(Context context){
+        if(sCrimeLab == null){
+            sCrimeLab = new CrimeLab(context);
+        }
+        return sCrimeLab;
+    }
+    //私有构造方法
+    private CrimeLab(Context context){
+        mCrimes = new ArrayList<>();
+        for(int i = 0;i < 100;i++){
+            Crime crime = new Crime();
+            crime.setTitle("Crime #" + i);
+            crime.setSolved(i % 2 == 0);
+            mCrimes.add(crime);
+        }
+    }
+    //返回列表
+    public List<Crime> getCrimes(){
+        return mCrimes;
+    }
+    //返回某个id对应的Crime对象
+    public Crime getCrime(UUID id){
+        for(Crime crime:mCrimes){
+            if(crime.getId().equals(id)){
+                return crime;
+            }
+        }
+        return null;
+    }
+}
